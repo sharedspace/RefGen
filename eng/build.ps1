158da0c02175d30106ab345254a01638c6b2867d
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2019-07-13 15:07:14 -0700

    Add build.ps1

--- eng/build.ps1
+++ eng/build.ps1
@@ -1,75 +1,132 @@
 class msbuild_finder {
-  [string]Get_ScriptDirectory(){
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-    return $Invocation.PSScriptRoot
-  }
+  [string]$RepoRoot = [IO.Directory]::GetParent($PSScriptRoot)
+  [string]$VsWhere
+  [string]$MsBuild
 
-  [string]Get_RepoRoot(){
-    $scriptDir = $this.Get_ScriptDirectory()
-   return [IO.Directory]::GetParent($scriptDir)
-  }
-
-  [string]Download_And_Get_VsWhere(){
+  msbuild_finder(){
+   # Find VsWhere 
    Install-Package -Name VsWhere -ProviderName Chocolatey -ForceBootstrap -Force
-   $vswhere = join-path (join-path $env:ChocolateyPath bin) vswhere.bat
-   return $vswhere
-  }
+   $this.VsWhere = join-path (join-path $env:ChocolateyPath bin) vswhere.bat
 
-  [void]setup_environment(){
-   $vswhere = $this.Download_And_Get_VsWhere()
+   # Set-up environment
+   $installationPath = & $this.VsWhere -prerelease -latest -property installationPath
+   #if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+   #  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
+   #  $name, $value = $_ -split '=', 2
+   #  set-content env:\"$name" $value
+   #  }
+   #}
 
-   $installationPath = & $vswhere -prerelease -latest -property installationPath
-   if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
-     & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
-     $name, $value = $_ -split '=', 2
-     set-content env:\"$name" $value
+   if ($installationPath){
+     $vsDevCmd = Join-Path $installationPath "Common7\Tools\vsdevcmd.bat"
+     if (Test-Path "$vsDevCmd") {
+      $vsDevCmd = '"' + $vsDevCmd + '"'
+      $process = New-Object System.Diagnostics.Process
+      $process.StartInfo.FileName = "${env:COMSPEC}"
+      $process.StartInfo.Arguments = "/s /c $vsDevCmd -no_logo && set"
+      $process.StartInfo.RedirectStandardOutput = $true
+      $process.StartInfo.UseShellExecute = $false
+      $process.StartInfo.CreateNoWindow = $false
+      $process.Start()
+      $process.WaitForExit()
+
+      $output = $process.StandardOutput.ReadToEnd() 
+      $output | ForEach-Object {
+       $name, $value = $_ -split '=', 2
+       set-content env:\"$name" $value
+      }
      }
    }
+
+   # Find MsBuild
+   $process = New-Object System.Diagnostics.Process
+   $process.StartInfo.FileName = $this.VsWhere
+   $process.StartInfo.Arguments = "-prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath"
+   $process.StartInfo.RedirectStandardOutput = $true
+   $process.StartInfo.UseShellExecute = $false
+   $process.Start()
+   $process.WaitForExit()
+  
+   # Try MSBuild versions 'Current' first, then look for version '15.0'
+   $versions = @('Current', '15.0')
+
+   if ($process.ExitCode -eq 0) {
+     $msBuildDir = $process.StandardOutput.ReadLine()
+     if ($msBuildDir) {
+       foreach ($version in $versions) {
+           $path = join-path $msBuildDir "MSBuild\$version\Bin\MSBuild.exe"
+           if (Test-Path -PathType Leaf "$path") {
+             $this.MsBuild = $path
+             break
+           }
+       }
+     }
+   }
+
+   if ($this.MsBuild -eq $null) {
+    throw "MsBuild.exe could not be found"
+   }
   }
 
-  [string]Get_MsBuild(){
-    # Try MSBuild versions 'Current' first, then look for version '15.0'
-    $versions = @('Current', '15.0')
+  #[string]Download_And_Get_VsWhere(){
+  # Install-Package -Name VsWhere -ProviderName Chocolatey -ForceBootstrap -Force
+  # $vswhere = join-path (join-path $env:ChocolateyPath bin) vswhere.bat
+  # return $vswhere
+  #}
 
-    $vswhere = $this.Download_And_Get_VsWhere()
-    $this.setup_environment()
-    $msBuild = $null
+  #[void]setup_environment(){
+  # $vswhere = $this.Download_And_Get_VsWhere()
+
+  # $installationPath = & $vswhere -prerelease -latest -property installationPath
+  # if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+  #   & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
+  #   $name, $value = $_ -split '=', 2
+  #   set-content env:\"$name" $value
+  #   }
+  # }
+  #}
+
+  #[string]Get_MsBuild(){
+  #  # Try MSBuild versions 'Current' first, then look for version '15.0'
+  #  $versions = @('Current', '15.0')
+
+  #  $vswhere = $this.Download_And_Get_VsWhere()
+  #  $this.setup_environment()
+  #  $msBuild = $null
  
-    $process = New-Object System.Diagnostics.Process
-    $process.StartInfo.FileName = $vswhere
-    $process.StartInfo.Arguments = "-prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath"
-    $process.StartInfo.RedirectStandardOutput = $true
-    $process.StartInfo.UseShellExecute = $false
-    $process.Start()
-    $process.WaitForExit()
+  #  $process = New-Object System.Diagnostics.Process
+  #  $process.StartInfo.FileName = $vswhere
+  #  $process.StartInfo.Arguments = "-prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath"
+  #  $process.StartInfo.RedirectStandardOutput = $true
+  #  $process.StartInfo.UseShellExecute = $false
+  #  $process.Start()
+  #  $process.WaitForExit()
   
-    if ($process.ExitCode -eq 0) {
-      $msBuildDir = $process.StandardOutput.ReadLine()
-      if ($msBuildDir) {
-        foreach ($version in $versions) {
-            $path = join-path $msBuildDir "MSBuild\$version\Bin\MSBuild.exe"
-            if (Test-Path -PathType Leaf "$path") {
-              $msbuild = $path
-              break
-            }
-        }
-      }
-    }
+  #  if ($process.ExitCode -eq 0) {
+  #    $msBuildDir = $process.StandardOutput.ReadLine()
+  #    if ($msBuildDir) {
+  #      foreach ($version in $versions) {
+  #          $path = join-path $msBuildDir "MSBuild\$version\Bin\MSBuild.exe"
+  #          if (Test-Path -PathType Leaf "$path") {
+  #            $msbuild = $path
+  #            break
+  #          }
+  #      }
+  #    }
+  #  }
 
-    if ($msBuild -eq $null) {
-     throw "MsBuild.exe could not be found"
-    }
+  #  if ($msBuild -eq $null) {
+  #   throw "MsBuild.exe could not be found"
+  #  }
 
-    return $msBuild
-   }
+  #  return $msBuild
+  # }
 
-   [void] build(){
-     $repoRoot = $this.Get_RepoRoot()
-     $msBuild = $this.Get_MsBuild()
-
-     & $msBuild $repoRoot\RefGen.Sln /t:restore /t:rebuild | Out-Default
-   }
+  [void] build(){
+    $solution = Join-Path $this.RepoRoot RefGen.Sln
+    & $this.MsBuild $solution /t:restore /t:rebuild | Out-Default
+  }
 }
 
 $finder = [msbuild_finder]::new()
-$finder.build()
+#$finder.build()
