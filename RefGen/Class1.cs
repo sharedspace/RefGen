8b0076a31ac795a8633c952648a1df63a9e31ae5
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-03-07 14:31:56 -0800

    Delete old sources

---
+++ RefGen/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefGen
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
