a97cae6237770c2c1773ecb35668a7f8bd8d3c38
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-03-14 19:42:43 -0700

    Add commandline processing

---
+++ RefGen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+
+namespace RefGen
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<FileInfo>(
+                    new string[] {"--input", "-i" },
+                    "Input Assembly"
+                    )
+            };
+
+            rootCommand.Description = "Reference Assembly Generator";
+            rootCommand.Handler = CommandHandler.Create<FileInfo>(ProcessCommandLineArgs);
+            return rootCommand.InvokeAsync(args).Result;
+        }
+
+        private static void ProcessCommandLineArgs([NotNull]FileInfo file)
+        {
+        }
+    }
+}
